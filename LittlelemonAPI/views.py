b2f1b83a820583093c3188983e0f75548b0d4569
ibrahimcheik <lecheikibra@outlook.fr>
2024-08-04 20:29:23 +0100

    generics views

--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+from rest_framework.decorators import api_view 
+
 # Create your views here.
+
+
+
+class MenuItemView(generics.ListCreateAPIView):
+
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
