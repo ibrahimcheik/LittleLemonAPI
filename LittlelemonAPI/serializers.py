35a08c17afd81c58d42ca09c74d9c4dae514c86d
ibrahimcheik <lecheikibra@outlook.fr>
2024-08-06 03:43:09 +0100

    Data Validation

--- LittlelemonAPI/serializers.py
+++ LittlelemonAPI/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import MenuItem, Category
 from decimal import Decimal
+#Validator
+from rest_framework.validators import UniqueValidator
+from rest_framework.validators import UniqueTogetherValidator
 
 
 
@@ -25,16 +28,60 @@ class MenuItemSerializer(serializers.HyperlinkedModelSerializer):
     category = CategorySerializers(read_only=True)
     category_id = serializers.IntegerField(write_only=True)
     
+    #price = serializers.DecimalField(max_digits=6, decimal_places=2, min_value=500)
+    
     """ category = serializers.HyperlinkedRelatedField(
         queryset = Category.objects.all(),
         view_name='category-detail'
         ) """
         
+        #Method:3 Using validate_field() method 
+    """ def validate_price(self, value):
+        if (value < 500):
+            raise serializers.ValidationError('Price should not be less than 500')
+    def validate_stock(self, value):
+        if (value < 0):
+            raise serializers.ValidationError('Stock cannot be negative') """
+            
+            
+        #Method 4: Using the validate() method 
+    def validate(self, attrs):
+        if(attrs['price']<500):
+            raise serializers.ValidationError('Price should not be less than 500')
+        if(attrs['inventory']<0):
+            raise serializers.ValidationError('Stock cannot be negative')
+        return super().validate(attrs)
+
+    #title = serializers.CharField(max_length=255,validators=[UniqueValidator(queryset=MenuItem.objects.all())])
+            
+        
     class Meta:
 
         model = MenuItem
         fields = ['id', 'title', 'price', 'stock', 'price_after_tax', 'category', 'category_id']
-        depth = 1
+        validators = [
+            UniqueTogetherValidator(
+                queryset=MenuItem.objects.all(),
+                fields=['title', 'price']
+                ),
+            ]
+        """ extra_kwargs = {
+            'title': {
+                'validators': [
+                    UniqueValidator(
+                        queryset=MenuItem.objects.all()
+                        )
+                    ]
+                }
+            } """  
+        
+        
+        """ extra_kwargs = {
+            'price': {'min_value': 500},
+            'stock':{'source':'inventory', 'min_value': 0}       
+            } """
+        
+        #depth = 1
         
     def calculate_tax(self, product=MenuItem):
         
